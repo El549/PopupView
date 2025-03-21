a5417d980e7e4c38e55a57694f578b625de3f1c5
大江山岚 <554357332@qq.com>
2025-03-21 17:15:55 +0800

    update

--- Sources/Internal/Managers/PopupManager.swift
+++ Sources/Internal/Managers/PopupManager.swift
@@ -16,6 +16,7 @@ public class PopupManager: ObservableObject {
     private(set) var popupsWithoutOverlay: [ID] = []
     private(set) var popupsToBeDismissed: [ID: DispatchSourceTimer] = [:]
     private(set) var popupActionsOnDismiss: [ID: () -> ()] = [:]
+    var popupScrollViewOffset: [ID: CGPoint] = [:]
 
     public static let shared: PopupManager = .init()
     
@@ -44,19 +45,66 @@ extension PopupManager {
     static func performOperation(_ operation: StackOperation) { DispatchQueue.main.async {
         removePopupFromStackToBeDismissed(operation)
         updateOperationType(operation)
+        switch operation {
+        case .insertAndReplace(let popup):
+            if shared.views.canBeInserted(popup) {
+                shared.popupScrollViewOffset[popup.id] = .zero
+                shared.scrollViewOffset = .zero
+            }
+        case .insertAndStack(let popup):
+            if shared.views.canBeInserted(popup) {
+                shared.popupScrollViewOffset[popup.id] = .zero
+                shared.scrollViewOffset = .zero
+            }
+        case .removeLast:
+            shared.popupScrollViewOffset.removeValue(forKey: shared.views.last?.id ?? .init())
+        case .remove(let id):
+            if let view = shared.views.first(where: { $0.id ~= id }) {
+                shared.popupScrollViewOffset.removeValue(forKey: view.id)
+            }
+        case .removeAllUpTo(let id):
+            //            if let index = lastIndex(where: predicate) { removeLast(count - index - 1) }
+            if let index = shared.views.lastIndex(where: { popup in
+                popup.id ~= id
+            }) {
+                let removeIndex = shared.views.count - index - 1
+                shared.popupScrollViewOffset.removeValue(forKey: shared.views[removeIndex].id)
+            }
+        case .removeAll:
+            shared.popupScrollViewOffset.removeAll()
+        }
         shared.views.perform(operation)
+        switch operation {
+        case .removeLast:
+            shared.scrollViewOffset = shared.popupScrollViewOffset[shared.views.last?.id ?? .init()] ?? .zero
+        case .remove(let id):
+            shared.scrollViewOffset = shared.popupScrollViewOffset[shared.views.last?.id ?? .init()] ?? .zero
+        case .removeAllUpTo(let id): break
+            shared.scrollViewOffset = shared.popupScrollViewOffset[shared.views.last?.id ?? .init()] ?? .zero
+        case .removeAll:
+            shared.scrollViewOffset = .zero
+        default: break
+        }
     }}
     static func dismissPopupAfter(_ popup: any Popup, _ seconds: Double) { shared.popupsToBeDismissed[popup.id] = DispatchSource.createAction(deadline: seconds) { performOperation(.remove(popup.id)) } }
     static func hideOverlay(_ popup: any Popup) { shared.popupsWithoutOverlay.append(popup.id) }
     static func onPopupDismiss(_ popup: any Popup, _ action: @escaping () -> ()) { shared.popupActionsOnDismiss[popup.id] = action }
 }
 private extension PopupManager {
-    static func removePopupFromStackToBeDismissed(_ operation: StackOperation) { switch operation {
-        case .removeLast: shared.popupsToBeDismissed.removeValue(forKey: shared.views.last?.id ?? .init())
-        case .remove(let id): shared.popupsToBeDismissed.removeValue(forKey: id)
-        case .removeAllUpTo, .removeAll: shared.popupsToBeDismissed.removeAll()
+    static func removePopupFromStackToBeDismissed(_ operation: StackOperation) {
+        switch operation {
+        case .removeLast:
+            shared.popupsToBeDismissed.removeValue(forKey: shared.views.last?.id ?? .init())
+            shared.popupScrollViewOffset.removeValue(forKey: shared.views.last?.id ?? .init())
+        case .remove(let id):
+            shared.popupsToBeDismissed.removeValue(forKey: id)
+            shared.popupScrollViewOffset.removeValue(forKey: id)
+        case .removeAllUpTo, .removeAll:
+            shared.popupsToBeDismissed.removeAll()
+            shared.popupScrollViewOffset.removeAll()
         default: break
-    }}
+        }
+    }
     static func updateOperationType(_ operation: StackOperation) {
         switch operation {
         case .insertAndReplace, .insertAndStack:
